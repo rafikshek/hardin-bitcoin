38b50e32140b0d8137800e3b0a0e1a87cf6dcb15
rafikshek <168984920+rafikshek@users.noreply.github.com>
2026-05-17 23:36:01 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,52 +1,60 @@
- 
-      export default function Home() {
+        "use client";
+
+export default function Home() {
+
+  const connectWallet = async () => {
+    if (window.ethereum) {
+      try {
+        await window.ethereum.request({
+          method: "eth_requestAccounts",
+        });
+
+        alert("Wallet Connected Successfully");
+      } catch (err) {
+        alert("Connection Cancelled");
+      }
+    } else {
+      alert("Please install MetaMask");
+    }
+  };
+
   return (
-    <main style={{
-      background:"#050816",
-      color:"white",
-      minHeight:"100vh",
-      padding:"40px",
-      fontFamily:"Arial"
-    }}>
-      
-      <h1 style={{
-        fontSize:"60px",
-        color:"#f59e0b",
-        marginBottom:"20px"
-      }}>
+    <main
+      style={{
+        background: "#050816",
+        color: "white",
+        minHeight: "100vh",
+        padding: "40px",
+        fontFamily: "Arial",
+      }}
+    >
+      <h1 style={{ color: "#f59e0b", fontSize: "60px" }}>
         HardinBitcoin
       </h1>
 
-      <h2 style={{
-        fontSize:"40px",
-        maxWidth:"700px",
-        lineHeight:"1.2"
-      }}>
+      <h2 style={{ fontSize: "42px", maxWidth: "700px" }}>
         Buy Bitcoin. Hold Secure. Grow With Confidence.
       </h2>
 
-      <p style={{
-        marginTop:"20px",
-        color:"#9ca3af",
-        maxWidth:"600px",
-        fontSize:"18px"
-      }}>
-        Hardin Bitcoin is a secure and smart platform to manage Bitcoin safely.
+      <p style={{ color: "#9ca3af", maxWidth: "600px" }}>
+        Secure Bitcoin platform with wallet connection.
       </p>
 
-      <button style={{
-        marginTop:"30px",
-        background:"#f59e0b",
-        color:"black",
-        border:"none",
-        padding:"15px 30px",
-        borderRadius:"10px",
-        fontSize:"18px",
-        fontWeight:"bold"
-      }}>
+      <button
+        onClick={connectWallet}
+        style={{
+          marginTop: "20px",
+          background: "#f59e0b",
+          color: "black",
+          border: "none",
+          padding: "15px 30px",
+          borderRadius: "10px",
+          fontWeight: "bold",
+          fontSize: "18px",
+        }}
+      >
         Connect Wallet
       </button>
-
     </main>
   );
 }
